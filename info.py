3826cd3c87f3f89dce88600ee3ddcdc0da1cac31
kalirajeshwaran <69472407+kalirajeshwaran@users.noreply.github.com>
2023-01-08 13:13:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "mongodb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001257426098'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'tamilfilmography1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
